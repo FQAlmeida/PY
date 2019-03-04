8a7c9c8c8629899e2f945b78e75ee3f5df41c8a2
Otávio Almeida <Otavioalmeida650@gmail.com>
2019-03-04 00:18:29 -0300

    Update joguinho do mal.py

--- joguinho do mal.py
+++ joguinho do mal.py
@@ -1,28 +1,30 @@
-#--coding:utf8;--
-#qpy:3
-#qpy:console
-import subprocess
-import sys
-e = input("Arquivo ou programa? \n").lower()
-if e == "programa":
-    while a == True :
-        print( "TAKSBAR" )
-        a = input( "Entre com o nome do programa \n" )
-        if a == "exit":
-            a = False
-        else:
-            subprocess.run( a )
-        continue
-elif e == "arquivos":
-    while s == True:
-        s = input("nome do arquivo\n")
-        if s == "exit":
-            s = False
-        else:
-            s.open()
-            s.read()
-            s.close()
-        continue
-else:
-    print("tchau")
-    sys.exit()
+import shlex, subprocess
+
+# Exit string options, parsed to lower
+exits = (option.lower() for option in ("Exit", "0", "Sair"))
+
+# Initialize the subprocess list
+running_subprocess_list = list()
+
+while True:
+    # Get users command
+    cmd = input("Digite o comando que você deseja executar:\n")
+    
+    # Verify if it isn't a exit command
+    if cmd.lower() in exits:
+        # Verify if exists subprocess alive
+        for running_subprocess in running_subprocess_list:
+            if running_subprocess and running_subprocess.pull() is None:
+                # If subprocess is alive, terminate the subprocess
+                running_subprocess.kill()
+        break
+    # Parse the command
+    cmd = shlex.split(cmd)
+    try:
+        # Try to execute the command
+        # This part needs more love to find programs that aren't in the PATH
+        running_subprocess = subprocess.Popen(cmd)
+        # Add the command to subprocess list
+        running_subprocess_list.append(running_subprocess)
+    except:
+        print("Comando inválido")
